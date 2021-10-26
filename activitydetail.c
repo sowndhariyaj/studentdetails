36f3246a0a4873ebfde5807c18c75384599f90ab
sowndhariyaj <sowndhariyaj.20cse@kongu.edu>
2021-10-26 18:05:30 +0530

    activity commit

---
+++ activitydetail.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+        int rollno;
+        int sem;
+        int no_papers_inside;
+        int no_projects_inside;
+        int no_papers_outside;
+        int no_projects_outside;
+        int no_prize_coding;
+        int no_prize_hackathon;
+        scanf("%d",&rollno);
+        scanf("%d",&sem);
+        scanf("%d",&no_papers_inside);
+        scanf("%d",&no_projects_inside);
+        scanf("%d",&no_papers_outside);
+        scanf("%d",&no_projects_outside);
+        scanf("%d",&no_prize_coding);
+        scanf("%d",&no_prize_hackathon);
+        printf("Rollno:%d",rollno);
+        printf("Semester:%d",sem);
+        printf("No of papers inside of KEC:%d",no_paper_inside);
+        printf("No of projects inside of KEC:%d",no_projects_inside);
+        printf("No of papers outside of KEC:%d",no_paper_outside);
+        printf("No of projects outside of KEC:%d",no_projects_outside);
+        printf("No of prizes in coding carnival:%d",no_prize_coding);
+        printf("No of prizes in hackathon:%d",no_prize_hackathon);
+}
